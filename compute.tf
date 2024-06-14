e48cf4be1bd828dcfb3bf7b17eda054508f8a0cd
ChaoYiChen <a951159aca@gmail.com>
2024-06-13 20:42:00 -0700

    update compute.tf for more instances

--- compute.tf
+++ compute.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "web1" {
   security_groups = [aws_security_group.web_sg.id]
   key_name      = aws_key_pair.generated_key.key_name
   tags = {
-    Name = "CCTB-ProductionEnv"
+    Name = "Production_Env1"
   }
 }
 
@@ -16,6 +16,41 @@ resource "aws_instance" "web2" {
   security_groups = [aws_security_group.web_sg.id]
   key_name      = aws_key_pair.generated_key.key_name
   tags = {
-    Name = "CCTB-ProductionEnv"
+    Name = "Production_Env2"
+  }
+}
+
+
+resource "aws_instance" "web2" {
+  ami           = "ami-0440d3b780d96b29d" # Amazon Linux 2 AMI (HVM)
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main_b.id
+  security_groups = [aws_security_group.web_sg.id]
+  key_name      = aws_key_pair.generated_key.key_name
+  tags = {
+    Name = "JenkinsController"
+  }
+}
+
+resource "aws_instance" "web2" {
+  ami           = "ami-0440d3b780d96b29d" # Amazon Linux 2 AMI (HVM)
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main_b.id
+  security_groups = [aws_security_group.web_sg.id]
+  key_name      = aws_key_pair.generated_key.key_name
+  tags = {
+    Name = "Testing_Env"
+  }
+}
+
+
+resource "aws_instance" "web2" {
+  ami           = "ami-0440d3b780d96b29d" # Amazon Linux 2 AMI (HVM)
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main_b.id
+  security_groups = [aws_security_group.web_sg.id]
+  key_name      = aws_key_pair.generated_key.key_name
+  tags = {
+    Name = "Staging_Env"
   }
 }
